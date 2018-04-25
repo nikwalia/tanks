85957e9a62342763ff96f7ff712fb1a34419f4e1
Your Name <you@example.com>
2018-04-25 16:38:27 -0700

    /**  *  * TODO Write a one-sentence summary of your class here. TODO Follow it with  * additional details about its purpose, what abstraction it represents, and how  * to use it.  *  * @author Maithreyee Vatsan  * @author Group members: Nikash Walia, Roopak Phatak  * @version Apr 25, 2018  * @author Period: TODO  * @author Assignment: Tanks  *  * @author Sources: TODO  */

--- requirementsM/MovableClassRequirements.java
+++ requirementsM/MovableClassRequirements.java
@@ -1,5 +1,19 @@
 package requirementsM;
 
+/**
+ * 
+ * TODO Write a one-sentence summary of your class here. TODO Follow it with
+ * additional details about its purpose, what abstraction it represents, and how
+ * to use it.
+ *
+ * @author Maithreyee Vatsan
+ * @author Group members: Nikash Walia, Roopak Phatak
+ * @version Apr 25, 2018
+ * @author Period: TODO
+ * @author Assignment: Tanks
+ *
+ * @author Sources: TODO
+ */
 public class MovableClassRequirements
 {
 
