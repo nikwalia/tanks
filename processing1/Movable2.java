3bb2cfe2d63a4e11bab5cdbec50063a10b481874
Your Name <you@example.com>
2018-04-25 16:11:55 -0700

    Movable class

---
+++ processing1/Movable2.java
@@ -0,0 +1,21 @@
+package processing1;
+
+// import_______________________ ;
+/**
+ * 
+ * TODO Write a one-sentence summary of your class here. TODO Follow it with
+ * additional details about its purpose, what abstraction it represents, and how
+ * to use it.
+ *
+ * @author Maithreyee Vatsan
+ * @author Group members: Nikash Walia, Roopak Phatak
+ * @version Apr 25, 2018
+ * @author Period: 2
+ * @author Assignment: APCS Project - Tanks
+ *
+ * @author Sources: None
+ */
+public class Movable2
+{
+
+}
