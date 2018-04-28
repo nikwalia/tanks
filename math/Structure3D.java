a77bd1a2075618bdfd19099173586de1cda9263e
nikwalia <nikash.walia@gmail.com>
2018-04-27 22:03:23 -0700

    working

--- math/Structure3D.java
+++ math/Structure3D.java
@@ -1,28 +1,24 @@
 package math;
-
-import javax.measure.quantity.Length;
-import javax.measure.unit.SI;
-
-import org.jscience.physics.amount.Amount;
+import math.Value3D;
 
 public class Structure3D extends Value3D
 {
-    private final Amount<Length> length;
-    private final Amount<Length> width;
+    private final double length;
+    private final double width;
  
     public Structure3D( int x, int y, int z, int angle, int l, int w )
     {
         super( x, y, z, angle );
-        length = Amount.valueOf( l, SI.METER );
-        width = Amount.valueOf( w, SI.METER );
+        length = l;
+        width = w;
     }
     
-    protected Amount<Length> getLength()
+    protected double getLength()
     {
         return length;
     }
     
-    protected Amount<Length> getWidth()
+    protected double getWidth()
     {
         return width;
     }
