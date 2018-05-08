26d7ffb22773e2be576a1bafe572bcd6f22c973e
Maithreyee Vatsan <you@example.com>
2018-05-08 09:09:45 -0700

    home screen commit

--- TypographywithHomeScreens/backgroundImage.java
+++ TypographywithHomeScreens/backgroundImage.java
@@ -1,30 +1,50 @@
 package TypographywithHomeScreens;
 
 import processing.core.PApplet;
+import processing.core.PFont;
 import processing.core.PImage;
+
 
 public class backgroundImage extends PApplet
 {
 
     public static void main( String[] args )
     {
-        PApplet.main( "TypographywithHomeScreens.backgroundImage");
+        PApplet.main( "TypographywithHomeScreens.backgroundImage" );
 
     }
 
     PImage background;
-    int back; 
+
+    // int background1;
+
+
     public void settings()
     {
-        size(1920, 1080);
+        size( 1920, 1080 );
     }
+
+
     public void setup()
     {
-      background = loadImage("C:\\Users\\mvats\\tanksactualforproject.jpg");
+        // PImage image;
+        background = loadImage( "C:\\Users\\mvats\\tanksactualforproject.jpg" );
+        noTint();
     }
+
+
     public void draw()
     {
-        background(background);
-        
+        // PFont font;
+        background( background );
+        textSize( 109 );
+        text( "T-34 Tank Destroyers", width / 4, 160 );
+        fill( 0 );
+        // font =
+        // createFont("C:\\Users\\mvats\\Downloads\\League-Gothic.zip\\LeagueGothic-Regular.otf")
+        // text( "word", 10, 60 );
+        // fill( 0, 102, 153, 51 );
+        // text( "word", 10, 90 );
+
     }
 }
