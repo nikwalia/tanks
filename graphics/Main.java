6060c8d9774aa6ece8514589a1512e06092b9a0e
Maithreyee Vatsan <you@example.com>
2018-05-27 16:51:28 -0700

    text fix for intro screen

--- graphics/Main.java
+++ graphics/Main.java
@@ -218,7 +218,7 @@ public class Main extends PApplet
         text( "Turret Left: [ Turret Right: ] Turret Fire: \\", width / 4, 10 * height / 21 );
         text( "Player Two Controls", width / 2 - 100, 10 * height / 17 );
         text( "Forward: W Backward: S Left: A Right: D", 2 * width / 7, 2 * height / 3 );
-        text( "Turret Left: ` Turret Left: 1 Turret Right: 2", width / 4, 5 * height / 7 );
+        text( "Turret Left: ` Turret Right: 1 Turret Fire: 2", width / 4, 5 * height / 7 );
         text( "Press Q to quit game", width / 2 - 50, 5 * height / 6 );
 
     }
@@ -331,6 +331,7 @@ public class Main extends PApplet
             gameOver( 1 );
         }
     }
+
 
     /**
      * checks whether or not either of the tanks have collided with a mine, and
